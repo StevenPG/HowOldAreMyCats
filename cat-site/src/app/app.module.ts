7e78ffad0f78c551db504cf50b7c4eb258899d2f
StevenPG <steven.p.gantz@gmail.com>
2021-02-07 01:20:01 -0500

    add current state, actually pretty decent so far

--- cat-site/src/app/app.module.ts
+++ cat-site/src/app/app.module.ts
@@ -10,21 +10,27 @@ import { ContentComponent } from './content/content.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 // Generic Components
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { InstagramfeedComponent } from './instagramfeed/instagramfeed.component';
+import { CatcardComponent } from './catcard/catcard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopbarComponent,
     ContentComponent,
+    InstagramfeedComponent,
+    CatcardComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatGridListModule,
+    MatCardModule,
     FlexLayoutModule
   ],
   providers: [],
---
+++ cat-site/src/app/instagramfeed/instagramfeed.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InstagramfeedComponent } from './instagramfeed.component';
+
+describe('InstagramfeedComponent', () => {
+  let component: InstagramfeedComponent;
+  let fixture: ComponentFixture<InstagramfeedComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InstagramfeedComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InstagramfeedComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
