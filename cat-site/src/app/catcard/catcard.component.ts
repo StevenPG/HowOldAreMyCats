67bc2d3ae238d53251e7bfac9b9ec580761e56f3
StevenPG <tyr@pop-os.localdomain>
2021-03-07 13:44:02 -0500

    update dates to make ages correct

--- cat-site/src/app/catcard/catcard.component.ts
+++ cat-site/src/app/catcard/catcard.component.ts
@@ -22,44 +22,52 @@ export class CatcardComponent implements OnInit {
   ngOnInit(): void {
     this.catBirthday = this.catBirthdate.toDateString()
 
-    let ageInMSec = Date.now() - this.catBirthdate.getTime()
-    let ageInSec = ageInMSec / 1000
-    let ageInMin = ageInSec / 60
-    let ageInHour = ageInMin / 60
-    let ageInDays = ageInHour / 24
-    let ageInWeeks = ageInDays / 7
-    let ageInMonths = ageInWeeks / 4
-    let ageInYear = ageInWeeks / 52
+    // Loop through fully and calculate for exact age
+    const testMonths = this.countMonths(this.catBirthdate);
+    const years = Math.floor(testMonths / 12);
+    const months = testMonths % 12;
 
-    let visibleMonthsAge = Math.floor(ageInMonths)
-    let visibleYearsAge = Math.floor(ageInYear)
-
-    if (visibleYearsAge == 0) {
-      if (visibleMonthsAge == 1) {
-        this.catAge = visibleMonthsAge + " month old"
+    if (years === 0) {
+      if (months === 1) {
+        this.catAge = months + ' month old';
       } else {
-        this.catAge = visibleMonthsAge + " months old"
+        this.catAge = months + ' months old';
       }
     } else {
-      let months = Math.floor(ageInMonths % 12)
+      let displayString = '';
 
-      let displayString = "";
-
-      if (visibleYearsAge == 1) {
-        displayString = displayString + visibleYearsAge + " year"
+      if (years === 1) {
+        displayString = displayString + years + ' year';
       } else {
-        displayString = displayString + visibleYearsAge + " years"
+        displayString = displayString + years + ' years';
       }
 
-      if (months == 0) {
-        displayString = displayString + " old"
-      } else if(months == 1) {
-        displayString = displayString + " " + months + " month old"
+      if (months === 0) {
+        displayString = displayString + ' old';
+      } else if(months === 1) {
+        displayString = displayString + ' ' + months + ' month old';
       } else {
-        displayString = displayString + " " + months + " months old"
+        displayString = displayString + ' ' + months + ' months old';
       }
 
       this.catAge = displayString
     }
+  }
+
+  public countMonths( birthDate: Date ): number {
+    const stepDate = new Date(birthDate);
+    const endDate = new Date(Date.now());
+    let monthCount = 0;
+
+    while ( stepDate <= endDate ) {
+      stepDate.setMonth(stepDate.getMonth() + 1);
+      monthCount += 1;
+    }
+
+    if ( stepDate !== endDate ) {
+      monthCount -= 1;
+    }
+
+    return monthCount;
   }
 }
