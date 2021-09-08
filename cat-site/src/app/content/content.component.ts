c913ff6c3419e096b369c80cb292c07effe15ecb
Steven Gantz <steven.p.gantz@gmail.com>
2021-09-07 23:43:32 -0400

    Update content.component.ts

--- cat-site/src/app/content/content.component.ts
+++ cat-site/src/app/content/content.component.ts
@@ -10,7 +10,7 @@ export class ContentComponent implements OnInit {
   // TODO - add background, nothing fancy
   eliName = 'Eli';
   eliNickname = 'Mr. Man, Bear, Woofie';
-  eliDescription = 'Eli was adopted on July 4th, 2010. He is the most riddled with health issues and has cost too much money over the last 4 years, but we love him anyway. He gets along with everyone and loves to chase the others around the house. He’s very vocal and makes the weirdest chirpy noises. He loves grass, catnip, and playing with your hands under blankets. His favorite toy are his “Kitty fun tubes”. He carries it around and cries to it, which gives me a heart attack because it sounds like he’s dying.';
+  eliDescription = 'Eli was adopted on July 4th, 2010. He is the most riddled with health issues and has cost too much money over the last 4 years, but we love him anyway. He gets along with everyone and loves to chase the others around the house. He’s very vocal and makes the weirdest chirpy noises. He loves grass, catnip, and playing with your hands under blankets. His favorite toy are his “Kitty fun tubes”. He carries it around and cries to it, which gives me a heart attack because it sounds like he’s dying. In mid August 2021, Eli was diagnosed with Sarcoma in his back right leg. On Sept 1st he had it removed and is now a tripaw-d!';
   // May 1st 2010
   eliBirthdate = new Date(2010, 4, 1);
   eliImageSrc = 'https://i.imgur.com/9uYa4LC.jpg';
@@ -25,7 +25,7 @@ export class ContentComponent implements OnInit {
   umbraImageAlt = 'An image of Umbra the cat';
 
   bucketName = 'Bucket';
-  bucketNickname = 'Buxter Brown, Bucky, Wings, Buckaroo';
+  bucketNickname = 'Buxter Brown, Bucky, Wings, Buckaroo, Booker';
   bucketDescription = 'Bucket was brought home on the day of the eclipse! He’s the weirdest one of the group. He has the strangest meow, oddest mannerisms, and is an all-around goofball! He lies to sit leaning against walls which makes him look like a walrus. Fun fact: Bucket has no hip bone! He fell into the tub and broke his femur head clean off, so he had surgery to have it completely removed. You can’t tell in the slightest (and I have his femur head in a container for keeps). He’s also a big fan of grass, and for some reason loves whistling. His favorite toy is his bird on a stick. It jingles when you swing it and he gets so excited. He carries it around and cries to it.';
   //  May 7, 2017
   bucketBirthdate = new Date(2017, 4, 7);
@@ -65,15 +65,15 @@ export class ContentComponent implements OnInit {
   milkImageAlt = 'An image of Milk the cat';
 
   bonesName = 'Bones';
-  bonesNickname = 'Boone, Booner, Chicken Bone';
-  bonesDescription = 'Bones was found right down the street from our house. He was only about 5 or 6 weeks old. When he was found he was covered in fleas, and so, so skinny (hence his name). Bones is the biggest chicken, and doesn’t like meeting new people. He is the biggest momma’s boy ever. He likes to jam his face into her neck and knead her. He has the tiniest meow, and is almost sounds like he’s crying when he does. He is not a huge fan of canned food, or treats, and only nibbles on his dry food. Bones was a foster fail, with initially no intention on keeping him, he became besties with Milk and we couldn’t let his weirdness leave. Bones’ favorite toy is his leather tassel toy. He loves to play tug of war with it like a dog.';
+  bonesNickname = 'Boone, Booner, Chicken Bone, Bo-Bones';
+  bonesDescription = 'Bones was found right down the street from our house. He was only about 5 or 6 weeks old. When he was found he was covered in fleas, and so, so skinny (hence his name). Bones is the biggest chicken, and doesn’t like meeting new people. He is the biggest momma’s boy ever. He likes to jam his face into her neck and knead her. He has the tiniest meow, and is almost sounds like he’s crying when he does. He is not a huge fan of canned food, or treats, and only nibbles on his dry food. Bones was a foster fail, with initially no intention on keeping him, he became besties with Milk and we couldn’t let his weirdness leave. Bones’ favorite toys are feet with socks on them and his leather tassel toy . He loves to play tug of war with it like a dog.';
   // September 9, 2019
   bonesBirthdate = new Date(2019, 8, 9);
   bonesImageSrc = 'https://i.imgur.com/rvWHEAo.jpg';
   bonesImageAlt = 'An image of Bones the cat';
 
   wobblesName = 'Wobbles';
-  wobblesNickname = 'Weebs, wobbs, Weebledy Wobbldy Woo';
+  wobblesNickname = 'Weebs, Wobbs, Weebledy Wobbldy Woo, Double W, Bob, Bobble';
   wobblesDescription = 'Weeble is the newest addition after a 2nd foster fail. We believe he has Cerebellar Hypoplasia, and he occasionally runs around looking like a little drunkard. He falls over from standing sometimes, and his head shakes. He was found feral with his sister at around 6 weeks old. He’s finally coming around, but it’s been a slow process. He still runs when you approach him too quickly, or if he thinks you might grab him. So far his favorite toy might be anything that moves.';
   // November 24th, 2020
   wobblesBirthdate = new Date(2020, 10, 24);
@@ -82,7 +82,7 @@ export class ContentComponent implements OnInit {
 
   pancakeName = 'Pancake';
   pancakeNickname = 'Cake, Panko Bread Crumbs, Hanky Panky, Hey-get-back-here-let-me-pet-you';
-  pancakeDescription = 'Along with Wobbles, Pancake is the newest addition after a 2nd foster fail. She absolutely loves the other cats, seeking them out and following them around until they give her attention. She was found feral with her brother at around 6 weeks old. She\'s very afraid of being picked up, but will let you pet her if you\'re slow and careful. She loves to play and is always chirping.';
+  pancakeDescription = 'Along with Wobbles, Pancake is the newest addition after a 2nd foster fail. She absolutely loves the other cats, seeking them out and following them around until they give her attention. She was found feral with her brother at around 6 weeks old. She\'s very afraid of being picked up, but will let you pet her if you\'re slow and careful. She loves to make little chirps, and will cry for your attention... but don\'t touch her! She will roll around and "flirt" with you, but trot away as soon as you get close. She\'s a sucker for Churu tubes though!';
   // November 24th, 2020
   pancakeBirthdate = new Date(2020, 10, 24);
   pancakeImageSrc = 'https://i.imgur.com/ybLxIrs.jpg';
