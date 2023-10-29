0a3b77a6ddd7ba719b4628b0ee3b2068d620f675
Steven PG <steven.p.gantz@gmail.com>
2023-10-29 16:29:09 -0400

    Started restructuring with more succinct format

--- new-cat-site/cat-site/src/App.tsx
+++ new-cat-site/cat-site/src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import './App.css';
-import init, { time_since} from "age-calc-lib";
+import init, {time_since} from "age-calc-lib";
 import BasicCard from "./app/catcard/Card";
 import ButtonAppBar from "./app/TopBar";
 
@@ -8,71 +8,41 @@ import textContent from "./app/contents.json"
 
 function App() {
 
-  // TODO - remove and add to lower card where calculation occurs
-  useEffect(() => {
-    init().then(() => {
-      console.log(time_since("2019-10-12T07:20:50.52Z"));
+    // TODO - remove and add to lower card where calculation occurs
+    useEffect(() => {
+        init().then(() => {
+            console.log(time_since("2019-10-12T07:20:50.52Z"));
+        })
     })
-  })
 
-  // TODO - add carousel to images in card: https://www.npmjs.com/package/react-responsive-carousel
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/*// TODO - rename and stretch across whole screen*/}
-        {/*// TODO - add a filter that will hide cards when you type in the filter*/}
-        <ButtonAppBar/>
-        {/* TODO Change this into a loop that passes base object if possible */}
-        {/*TODO - make cards side by side with space in between the cards*/}
-        <BasicCard
-            name={textContent.cards.eli.name}
-            // TODO - get this from a method call that adds a list
-            nicknames={textContent.cards.eli.nicknames}
-            description={textContent.cards.eli.description}
-        />
-        <BasicCard
-            name={textContent.cards.umbra.name}
-            description={textContent.cards.umbra.description}
-        />
-        <BasicCard
-            name={textContent.cards.bucket.name}
-            description={textContent.cards.bucket.description}
-        />
-        <BasicCard
-            name={textContent.cards.bitty.name}
-            description={textContent.cards.bitty.description}
-        />
-        <BasicCard
-            name={textContent.cards.bones.name}
-            description={textContent.cards.bones.description}
-        />
-        <BasicCard
-            name={textContent.cards.pixie.name}
-            description={textContent.cards.pixie.description}
-        />
-        <BasicCard
-            name={textContent.cards.milk.name}
-            description={textContent.cards.milk.description}
-        />
-        <BasicCard
-            name={textContent.cards.bones.name}
-            description={textContent.cards.bones.description}
-        />
-        <BasicCard
-            name={textContent.cards.wobble.name}
-            description={textContent.cards.wobble.description}
-        />
-        <BasicCard
-            name={textContent.cards.pancake.name}
-            description={textContent.cards.pancake.description}
-        />
-        <BasicCard
-            name={textContent.cards.triscuit.name}
-            description={textContent.cards.triscuit.description}
-        />
-      </header>
-    </div>
-  );
+    let cardJson = JSON.parse(textContent["cards"])
+
+    let cardList = cardJson.map(
+        (element: {
+            name: string;
+            nicknames: string;
+            description: string; }, index: number) => (
+            <BasicCard
+                name={element.name}
+                // TODO - get this from a method call that adds a list
+                nicknames={element.nicknames}
+                description={element.description}
+            />
+        )
+    );
+
+    // TODO - add carousel to images in card: https://www.npmjs.com/package/react-responsive-carousel
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/*// TODO - rename and stretch across whole screen*/}
+                {/*// TODO - add a filter that will hide cards when you type in the filter*/}
+                <ButtonAppBar/>
+                {/* TODO Change this into a loop that passes base object if possible */}
+                {/*TODO - make cards side by side with space in between the cards*/}
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- new-cat-site/cat-site/src/app/TopBar.tsx
+++ new-cat-site/cat-site/src/app/TopBar.tsx
@@ -3,27 +3,29 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 
 import textContent from "./contents.json";
-import {makeStyles} from "@mui/material";
-import getTextDecoration from "@mui/material/Link/getTextDecoration";
 
 export default function ButtonAppBar() {
-        return (
+    // TODO - remove the ts-ignore and fix the error
+        // @ts-ignore
+    let initialText = textContent["top-bar"]["main-text"].beginning;
+
+    return (
         <Box>
             <AppBar style={{width: "100%"}}>
                 <Toolbar>
                     {/* Align text with center of screen */}
                     <Typography variant="h5" component="div">
-                        { textContent["top-bar"]["main-text"].beginning }
+                        { initialText }
                     </Typography>
                     <Typography variant="h5" component="div" style={{textDecoration: "line-through"}}>
+                        {/*@ts-ignore*/}
                         { textContent["top-bar"]["main-text"]["struck-through"] }
                     </Typography>
                     {/* TODO insert a space */}
                     <Typography variant="h5" component="div">
+                        {/*@ts-ignore*/}
                         { textContent["top-bar"]["main-text"].end}
                     </Typography>
                     {/* // TODO put strike-thrus */}
