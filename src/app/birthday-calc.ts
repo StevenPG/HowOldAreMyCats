eeb6c087c17414f47899ca55e9bc3584821315f6
Steven Gantz <steven.p.gantz@gmail.com>
2025-12-18 02:44:10 -0500

    Push new birthday calculation

--- src/app/birthday-calc.ts
+++ src/app/birthday-calc.ts
@@ -1,6 +1,14 @@
 import textContent from "./contents.json";
 
 /**
+ * Checks if the deathdate is empty or undefined
+ * @param deathdate the deathdate of the cat
+ */
+function isAlive(deathdate: string): boolean {
+    return deathdate === "" || deathdate === undefined;
+}
+
+/**
  * This takes in the entire props structure and manipulates incoming string data to generate
  * a formatted output to be directly displayed.
  * @returns the next birthday details as a string used for the banner of the page
@@ -9,19 +17,27 @@ export function nextBirthdayAsStr(): string {
     let soonestName: string = ""
     let soonestDays: number = 99999
     textContent.cards.forEach(details => {
-        console.log(details.deathdate);
-        if(details.deathdate === "" || details.deathdate === undefined) {
-            const date = new Date(details.birthdate);
+        // Difference between todays date and birthdate
+        let diffDays;
+
+        if(isAlive(details.deathdate)) {
+            const birthDayDate = new Date(details.birthdate);
             const todaysDate = new Date();
 
-            date.setFullYear(2000);
-            todaysDate.setFullYear(2000);
+            // Assign birthday to the current year to test next birthday
+            birthDayDate.setFullYear(todaysDate.getFullYear());
 
-            let diff = date.getTime() - todaysDate.getTime()
-            var diffDays = Math.ceil(diff / (1000 * 3600 * 24));
+            const dateDiff = birthDayDate.getTime() - todaysDate.getTime()
+            if(dateDiff < 0) {
+                // Birthday has already passed so we recalculate for next year
+                birthDayDate.setFullYear(todaysDate.getFullYear() + 1);
+                const dateDiffNextYear = birthDayDate.getTime() - todaysDate.getTime()
+                diffDays = Math.ceil(dateDiffNextYear / (1000 * 3600 * 24));
+            } else {
+                diffDays = Math.ceil(dateDiff / (1000 * 3600 * 24));
+            }
 
-            // Number of diff days is in the future, and is less than current soonest days
-            if (diffDays > 0 && diffDays < soonestDays) {
+            if (diffDays < soonestDays) {
                 soonestName = details.name
                 soonestDays = diffDays
             }
